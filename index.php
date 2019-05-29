16cbbd01043e96cbbe3e7ea002bc361b3b1bf9fa
mariaignaciaclark <48286052+mariaignaciaclark@users.noreply.github.com>
2019-05-29 12:42:53 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include('cabeza.php')?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+
+    <h1><?php print $title;?></h1>
+        
+    <h3><?php print basename($_SERVER['PHP_SELF']);?></h3>
+
+      <img src="https://raw.githubusercontent.com/mariaignaciaclark/ev2/gh-pages/imagenes/trabajo-<?php print(rand(1,3));?>.jpg">
+    </div>
+  </div>
+</div>
+
+  <?php include('patas.php')?>
